307e83ca00467cc893c68d583e699cf615d16314
Santiago Ruiz <hiebra@hotmail.com>
2021-10-13 10:31:12 +0200

    formatting (eclipse)

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ import { getInput, setOutput, setFailed } from '@actions/core';
 import { context } from '@actions/github';
 
 try {
-  const nameToGreet = getInput('who-to-greet'); //kk
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  const ebook = new EpubPress({
-    title: 'Abiogenesis',
-    description: 'eBook-friendly mirror of the Wikipedia article',
-    urls: [
-        'http://es.wikipedia.org/Abiogenesis'
-    ]
-  });
+	const nameToGreet = getInput('who-to-greet'); //kk
+	console.log(`Hello ${nameToGreet}!`);
+	const time = (new Date()).toTimeString();
+	setOutput("time", time);
+	// Get the JSON webhook payload for the event that triggered the workflow
+	const payload = JSON.stringify(context.payload, undefined, 2)
+	console.log(`The event payload: ${payload}`);
+	const ebook = new EpubPress({
+		title: 'Abiogenesis',
+		description: 'eBook-friendly mirror of the Wikipedia article',
+		urls: [
+			'http://es.wikipedia.org/Abiogenesis'
+		]
+	});
 } catch (error) {
-  setFailed(error.message);
+	setFailed(error.message);
 }
